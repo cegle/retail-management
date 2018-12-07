9de6df699944d86a9713529725a14c1252c5ca20
cegle <cegle@qq.com>
2018-12-07 10:23:15 +0800

    日常更新

---
+++ src/components/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-container>
+    <el-main>
+      <div class="login">login</div>
+    </el-main>
+  </el-container>
+</template>
+
+<style lang="scss" scoped>
+el-container {
+  .login {
+    width: 100%;
+    background-color: red;
+    background-image: url(../assets/login/login_bg.png);
+  }
+}
+</style>
